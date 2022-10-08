1c44f069d648ff84890512bae57cc264196729d5
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2022-10-08 22:50:57 +0500

    adds custom script to import documents for the resort collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
-
 module.exports = {
   siteMetadata: {
     title: `typesense-demo-example`,
@@ -20,6 +19,61 @@ module.exports = {
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GATSBY_GA_TRACKING_ID,
+      },
+    },
+    "gatsby-plugin-image",
+    {
+      resolve: `gatsby-plugin-typesense`,
+      options: {
+        rootDir: `${__dirname}/public`, // Required
+        collectionSchema: {
+          // Required
+          name: "resorts",
+          fields: [
+            {
+              name: "name",
+              type: "string",
+              facet: true,
+            },
+            {
+              name: "numberOfRooms",
+              type: "float",
+            },
+            // {
+            //   name: "description",
+            //   type: "string",
+            // },
+            {
+              name: "tags",
+              type: "string[]",
+              optional: true,
+              facet: true,
+            },
+            { name: "location", type: "string", optional: true },
+            {
+              name: "page_path", // Required
+              type: "string",
+              optional: true,
+            },
+            {
+              name: "page_priority_score", // Required
+              type: "int32",
+              optional: true,
+            },
+          ],
+          default_sorting_field: "numberOfRooms", // Required
+        },
+        server: {
+          // Required
+          apiKey: "xyz",
+          nodes: [
+            {
+              host: "localhost",
+              port: "8108",
+              protocol: "http",
+            },
+          ],
+        },
       },
     },
   ],
